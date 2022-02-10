77725580305a4ea64d4a396b0aee44d11626644b
treastrain / Tanaka Ryoga <tr@tret.jp>
2022-02-10 11:24:45 +0900

    Update UIViewController.swift

--- Sources/SnapshotTesting/Snapshotting/UIViewController.swift
+++ Sources/SnapshotTesting/Snapshotting/UIViewController.swift
@@ -11,11 +11,13 @@ extension Snapshotting where Value == UIViewController, Format == UIImage {
   ///
   /// - Parameters:
   ///   - config: A set of device configuration settings.
+  ///   - drawHierarchyInKeyWindow: Utilize the simulator's key window in order to render `UIAppearance` and `UIVisualEffect`s. This option requires a host application for your tests and will _not_ work for framework test targets.
   ///   - precision: The percentage of pixels that must match.
   ///   - size: A view size override.
   ///   - traits: A trait collection override.
   public static func image(
     on config: ViewImageConfig,
+    drawHierarchyInKeyWindow: Bool = false,
     precision: Float = 1,
     size: CGSize? = nil,
     traits: UITraitCollection = .init()
@@ -25,7 +27,7 @@ extension Snapshotting where Value == UIViewController, Format == UIImage {
       return SimplySnapshotting.image(precision: precision, scale: traits.displayScale).asyncPullback { viewController in
         snapshotView(
           config: size.map { .init(safeArea: config.safeArea, size: $0, traits: config.traits) } ?? config,
-          drawHierarchyInKeyWindow: false,
+          drawHierarchyInKeyWindow: drawHierarchyInKeyWindow,
           traits: traits,
           view: viewController.view,
           viewController: viewController
